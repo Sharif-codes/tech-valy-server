95c9417adad9018f0efaab1b1fc6e4abd6434c8d
sharif-codes <sharifxenjia@gmail.com>
2023-10-18 18:34:18 +0600

    password and username hidden and vercel json file added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 console.log(process.env.DB_USER)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://tech-valy-server:exhCBPPWIUxDC1cp@cluster0.nh9hntn.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nh9hntn.mongodb.net/?retryWrites=true&w=majority`;
 console.log(process.env.DB_USER)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
